a374e947d2765fcfcf11ac19fe9768044cad9cc6
minzhuo huang <huanmingzhuo@gmail.com>
2024-08-15 02:09:43 +0800

    Add files via upload

---
+++ huanmingzhuo/js/DFPWM.js
@@ -0,0 +1,10 @@
+function darkMode() {
+    let element = document.body;
+    element.className = "dark-mode";
+    content.innerText = "Dark Mode is ON";
+}
+function lightMode() {
+    let element = document.body;
+    element.className = "light-mode";
+    content.innerText = "Dark Mode is OFF";
+}
