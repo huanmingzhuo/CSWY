b1586ba22bf392ab984af773d4d8673d3b7679ec
minzhuo huang <huanmingzhuo@gmail.com>
2024-08-15 02:09:20 +0800

    Create main.js

---
+++ huanmingzhuo/js/main.js
@@ -0,0 +1,12 @@
+function darkMode() {
+    let element = document.body;
+    let content = document.getElementById("DarkModetext");
+    element.className = "dark-mode";
+    content.innerText = "Dark Mode is ON";
+}
+function lightMode() {
+    let element = document.body;
+    let content = document.getElementById("DarkModetext");
+    element.className = "light-mode";
+    content.innerText = "Dark Mode is OFF";
+}
