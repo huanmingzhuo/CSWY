dd78b7616ac5237fdd2b35a8951dddd62bf2a557
minzhuo huang <huanmingzhuo@gmail.com>
2024-08-19 17:08:38 +0800

    Add files via upload
    增加复制功能

--- huanmingzhuo/js/main.js
+++ huanmingzhuo/js/main.js
@@ -2,11 +2,19 @@ function darkMode() {
     let element = document.body;
     let content = document.getElementById("DarkModetext");
     element.className = "dark-mode";
-    content.innerText = "Dark Mode is ON";
+    content.innerText = "现在是黑暗模式";
 }
 function lightMode() {
     let element = document.body;
     let content = document.getElementById("DarkModetext");
     element.className = "light-mode";
-    content.innerText = "Dark Mode is OFF";
+    content.innerText = "现在是明亮模式";
+}
+
+function copyLink(url) {  
+  navigator.clipboard.writeText(url).then(function() {  
+    alert('链接已复制到剪贴板');  
+  }, function(err) {  
+    console.error('复制失败: ', err);  
+  });  
 }
